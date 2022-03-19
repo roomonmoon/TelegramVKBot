f6a8d81158a2ad08a3ce398d96b2a289d8405af4
Sergey Degtyar <68108301+roomonmoon@users.noreply.github.com>
2022-03-20 01:35:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import telebot
 import vk_api
 import requests
 import json
+import time
 
 vk_session = vk_api.VkApi(f'{login}', f'{password}')
 vk_session.auth()
@@ -19,23 +20,22 @@ def telegram_bot(bot_token):
 
     @bot.message_handler(content_types=["text"])
     def send_text(message):
-        if len(message.text) < 10 and message.text.isdigit():
-            messageID = message.text
+        while True:
             try:
-                getUsers = vk.users.get(user_id=f"{messageID}", fields='last_seen,followers_count,photo_200_orig',)[0]
+                getUsers = vk.users.get(user_id="461232958", fields='last_seen,followers_count,photo_200_orig',)[0]
                 userID = getUsers['id']
                 userPhoto = getUsers['photo_200_orig']
                 fullname = getUsers['first_name'] + ' ' + getUsers['last_name']
                 lastseen = datetime.utcfromtimestamp(getUsers['last_seen']['time'] + 10800).strftime('%Y-%m-%d %H:%M')
 
-                GetFollowers = vk.users.getFollowers(user_id=f"{messageID}")
+                GetFollowers = vk.users.getFollowers(user_id="461232958")
                 followersCount = GetFollowers['count']
                 followers = GetFollowers['items']
                 LastFollower = vk.users.get(user_id=f"{followers[0]}", fields='id, first_name, last_name')[0]
                 IDLastFollower = followers[0]
                 fullnameLastFollower = LastFollower['first_name'] + ' ' + LastFollower['last_name']
 
-                GetFriends = vk.friends.get(user_id=f"{messageID}")
+                GetFriends = vk.friends.get(user_id="461232958")
                 friendsCount = GetFriends['count']
                 friends = GetFriends['items']
                 LastFriend = vk.users.get(user_id=f"{friends[0]}", fields='id, first_name, last_name')[0]
@@ -43,12 +43,12 @@ def telegram_bot(bot_token):
                 fullnameLF = LastFriend['first_name'] + ' ' + LastFriend['last_name']
 
                 bot.send_message(message.chat.id, 
-f"ID пользователя: {userID}\nИмя пользователя: {fullname}\nПоследний раз в сети: {lastseen}\n\nОбщее кол-во друзей: {friendsCount}\nОбщее кол-во подписчиков: {followersCount}\n\nПоследний друг: {fullnameLF}\nПоследний подписчик: {fullnameLastFollower}\n{userPhoto}"
+f"ID пользователя: {userID}\nИмя пользователя: {fullname}\nПоследний раз в сети: {lastseen}\n\nОбщее кол-во друзей: {friendsCount}\nОбщее кол-во подписчиков: {followersCount}\n\nПоследний друг: {fullnameLF}\nПоследний подписчик: {fullnameLastFollower}\n\nДанные на {datetime.now().strftime('%Y-%m-%d %H:%M')}\n\n{userPhoto}"
                 )       
             except Exception as ex: 
                 bot.send_message(message.chat.id, "Что-то пошло не так!")
-        else:
-            bot.send_message(message.chat.id, "Невалидный ID!")
+            time.sleep(9000) 
+            ################
     bot.polling()
 
 def main():
